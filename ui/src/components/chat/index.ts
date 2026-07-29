decc5f9a304b3ad4dd609ef2c6a34ac6c97d71d8
liuloulou <leo.adonia@gmail.com>
2026-07-29 15:08:17 +0800

    chore: refine useChat and useChatSession

--- ui/src/components/chat/index.ts
+++ ui/src/components/chat/index.ts
@@ -4,11 +4,12 @@ export {
   useAnnotationSources,
 } from "./Annotation";
 export { Conversation } from "./Chat";
-export { ChatProvider, useChat } from "./ChatContext";
+export { useChat } from "./ChatContext";
 export {
   ChatScrollContainerContext,
   useChatScrollContainer,
 } from "./ChatScrollContext";
+export { ChatSessionProvider, useChatSession } from "./ChatSessionContext";
 export { Sender } from "./Sender";
 export type { Message, MessageRole } from "./types";
 export { WebSearchTool } from "./WebSearchTool";
